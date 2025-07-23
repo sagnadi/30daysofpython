c57c741ebbcc3d3e8aeb38d4f58211c998e44085
sagnadiofficiel <sagnadiofficiel@gmail.com>
2025-07-23 23:32:21 +0000

    Exercice3 du premier jour

---
+++ Day_1/exrcice3.py
@@ -0,0 +1,18 @@
+# 1 Exemple pour différents types de données Python 
+
+print (459) # int 
+print (76.1478) # float 
+print (13 + 5j) #complexe
+print ('asabeneh') # string
+print (True) 
+print ([45, 76, 105]) # list 
+print ((9.8, 3.14, 2.7)) # tuple
+print ({0, 56, 2, 12}) # set
+print ({'name': 'asabeneh'}) # dictionary
+
+
+# 2 Calcul des distances euclidienne
+
+print(abs(2 - 3)) # Distance euclidienne entre (2, 3)
+
+print(abs(10 - 8)) # Distance euclidienne entre (10, 8)
