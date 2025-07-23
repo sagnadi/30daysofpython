8a7286033438cd8f95ec65c3dba6c69950bea6b9
sagnadiofficiel <sagnadiofficiel@gmail.com>
2025-07-23 23:08:46 +0000

    Exercice du premier jour

---
+++ Day_1/helloworld.py
@@ -0,0 +1,33 @@
+#Day_1 - 30DaysOfPython Challenge
+
+#Exercice 1
+
+import sys 
+print(sys.version) #Version de python
+
+#Exercie 2
+
+print(3 + 4) # addition(+)
+print(3 - 4) # subtraction(-)
+print(3 * 4) # multiplication(*)
+print(3 / 4) # division(/)
+print(3 ** 4) # exponential(**)
+print(3 % 4) # modulus(%)
+print(3 // 4) # Floor division operator(//)
+
+#Exercice 3
+
+print("Maxime") #Mon nom
+print("ALEDI") #Mon prenom
+print("TOGO") #Mon pays
+print("je profite de 30 joursde python")
+
+#Exercice 4
+
+print (type (10))   
+print (type (9.8))   
+print (type (3.14))   
+print (type (4 + 4j)) 
+print (type (['Asabeneh', 'Python', 'Finlande']))
+print (type ("ALEDI")) 
+print (type ("TOGO")) 
